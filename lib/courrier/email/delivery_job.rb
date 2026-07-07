f1feb0a45b7b7d7630998cf2392a262ef0efb063
e <988051+eelcoj@users.noreply.github.com>
2026-07-07 13:37:13 +0000

    Added support for ActiveSupport::Notifications (instrumentation) (#4)

--- lib/courrier/email/delivery_job.rb
+++ lib/courrier/email/delivery_job.rb
@@ -4,7 +4,12 @@ module Courrier
   class Email
     class DeliveryJob < ActiveJob::Base
       def perform(email_class_name, **options)
-        Object.const_get(email_class_name).new(**options).deliver_now
+        Object.const_get(email_class_name).new(**options).deliver_now.tap do
+          ActiveSupport::Notifications.instrument("delivery.courrier", email: email_class_name, options: options)
+        end
+      rescue => exception
+        ActiveSupport::Notifications.instrument("delivery_failed.courrier", email: email_class_name, options: options, exception: [exception.class.name, exception.message])
+        raise
       end
     end
   end
