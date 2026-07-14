224a26de49303409ccc608b5a21105661438c4c3
e <988051+eelcoj@users.noreply.github.com>
2026-07-14 14:24:08 +0000

    Provider-aware generator (#7)

--- lib/generators/courrier/install_generator.rb
+++ lib/generators/courrier/install_generator.rb
@@ -4,8 +4,18 @@ module Courrier
 
     source_root File.expand_path("templates", __dir__)
 
+    class_option :provider, type: :string, desc: "Email delivery provider (#{Courrier::Email::Provider::PROVIDERS.keys.join(", ")})"
+
     def copy_initializer_file
       template "initializer.rb", "config/initializers/courrier.rb"
+    end
+
+    private
+
+    def provider_config_options
+      provider_class = Courrier::Email::Provider::PROVIDERS[options[:provider]&.to_sym]
+
+      provider_class ? provider_class.config_options : []
     end
   end
 end
